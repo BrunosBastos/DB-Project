4493d597927c407a5c94354056dc53aeecb0369f
unknown <brunosb@ua.pt>
2020-05-27 22:56:21 +0100

    Update Interface: Company Details

--- App/CompanyDetails.cs
+++ App/CompanyDetails.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace App
 {
@@ -24,7 +25,36 @@ namespace App
         {
             // query to get the details of the company
 
+            if (Program.verifySGBDConnection())
+            {
+                SqlCommand cmd = new SqlCommand("Select * From Project.udf_getCompanyDetails(" + IDCompany + ")", Program.cn);
+                SqlDataReader reader = cmd.ExecuteReader();
+                reader.Read();
+
+                CName.Text = reader["CompanyName"].ToString();
+                Contact.Text = reader["Contact"].ToString();
+                Website.Text = reader["Website"].ToString();
+                Country.Text = reader["Country"].ToString();
+                City.Text = reader["City"].ToString();
+                FoundationDate.Text = reader["FoundationDate"].ToString().Split(' ').ToArray()[0];
+                if (!reader.IsDBNull(4))
+                {
+                    Logo.LoadAsync(reader["logo"].ToString());
+                }
+
+
+
+
+                reader.Close();
+
+            }
+
+
         }
 
+        private void Close(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
