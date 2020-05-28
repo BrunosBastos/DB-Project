e3169e40d83b90121a318d802cd172a42a46a9d3
unknown <brunosb@ua.pt>
2020-05-28 19:08:06 +0100

    AddAll com as novas udfs

--- Reset/AddAll.sql
+++ Reset/AddAll.sql
@@ -1,4 +1,4 @@
---USE LocalDB
+USE LocalDB
 CREATE SCHEMA Project;
 go
 
@@ -1224,6 +1224,30 @@ AS
 			return ( select COUNT(*) from @temp WHERE SerialNum IS NULL)
 		END
 GO
+go
+create Function Project.udf_getGenreDetails(@GenName Varchar(25)) Returns Table
+as
+	Return( Select * From Project.Genre where Genre.GenName=@GenName);
+go
+go
+Create Function Project.udf_getGenreGames(@GenName Varchar(25)) Returns Table
+as
+	Return ( Select Name From Project.GameGenre Join Project.Game ON GameGenre.IDGame=Game.IDGame where GenName=@GenName);
+
+go
+go
+Create Function Project.udf_getNumberGenreGames(@GenName Varchar(25)) Returns int
+as
+	begin
+	declare @temp as int
+	SELECT @temp=COUNT(Name) FROM Project.udf_getGenreGames(@GenName);
+	return @temp;
+	end
+go
+
+
+
+
 
 
 GO
