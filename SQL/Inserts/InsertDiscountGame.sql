50dea2f4f906c937e896db417680a5e2c4ca2167
unknown <brunosb@ua.pt>
2020-06-12 13:59:36 +0100

    Menos uma pasta

--- SQL/Inserts/InsertDiscountGame.sql
+++
@@ -1,6 +0,0 @@
-
-INSERT INTO Project.DiscountGame(PromoCode,IDGame) VALUES(1,1);
-INSERT INTO Project.DiscountGame(PromoCode,IDGame) VALUES(2,2);
-INSERT INTO Project.DiscountGame(PromoCode,IDGame) VALUES(3,3);
-INSERT INTO Project.DiscountGame(PromoCode,IDGame) VALUES(1,4);
-
